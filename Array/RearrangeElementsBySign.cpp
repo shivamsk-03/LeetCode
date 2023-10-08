173e6ef3915a7f5179efad118c81f900032ecaad
Shivam Singh Khatri <83295597+shivamsk-03@users.noreply.github.com>
2023-10-08 17:53:48 +0530

    Add files via upload

--- Array/RearrangeElementsBySign.cpp
+++ Array/RearrangeElementsBySign.cpp
@@ -1,3 +1,4 @@
+//Solution 1
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
@@ -10,6 +11,26 @@ public:
         for(int i=0;i<nums.size()/2;i++){
             ans.push_back(p[i]);
             ans.push_back(n[i]);
+        }
+        return ans;
+    }
+};
+//Solution 2
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>ans(nums.size());
+        int posi=0;
+        int neg=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0){
+                ans[posi]=nums[i];
+                posi+=2;
+            }
+            else{
+                ans[neg]=nums[i];
+                neg+=2;
+            }
         }
         return ans;
     }
